3119bc8e325806669e890d0ed72e0987955a1778
bisharaf <python.developers.ir@gmail.com>
2023-07-30 13:25:12 +0330

    using class_based view for create and upodate and delete post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-
-
+from .views import PostListView, PostDetailView , PostCreateView, PostUpdateView, PostDeleteView
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about')
+    path('', PostListView.as_view(), name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+    path('post/<pk>', PostDetailView.as_view(), name='blog_detail'),
+    path('post/<pk>/update', PostUpdateView.as_view(), name='blog-update'),
+    path('post/<pk>/delete', PostDeleteView.as_view(), name='blog-delete'),
+    path('posts/new/', PostCreateView.as_view(), name='blog-create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
+from typing import Optional
+from django.forms.models import BaseModelForm
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 
 # posts = [
 #     {
@@ -18,12 +24,63 @@ from .models import Post
 # ]
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'blog/home.html', context)
 
+
+class PostListView(ListView):
+    model =Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView): 
+    model = Post
+    fields= ['title', 'content']
+    success_url = reverse_lazy('blog-home')
+    def form_valid(self, form):
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
+
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): 
+    model = Post
+    fields= ['title', 'content']
+    success_url = reverse_lazy('blog-home')
+    def form_valid(self, form):
+        form.instance.author = self.request.user 
+        return super().form_valid(form)
+
+
+    def test_func(self):
+        post = self.get_object()
+        if post.author == self.request.user:
+            return True
+        else:
+            return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if post.author == self.request.user:
+            return True
+        else:
+            return False
+
+ 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'about'})
